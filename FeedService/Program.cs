c7446edb4cdaa0a25aea788863db11251768609e
H1Kt0 <konovalchuk.denis@gmail.com>
2024-09-09 17:49:14 +0300

    добавил свагер

--- FeedService/Program.cs
+++ FeedService/Program.cs
@@ -6,10 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<Context>(options =>
 	options.UseSqlServer("Server=localhost;Database=Finalah;Trusted_Connection=True;TrustServerCertificate=True"));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
