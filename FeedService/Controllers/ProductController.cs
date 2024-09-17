3033863d441c29844ebb6f9891507e2b12ad320b
H1Kt0 <konovalchuk.denis@gmail.com>
2024-09-17 15:50:12 +0300

    добавил контроллер для продуктов

---
+++ FeedService/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using FeedService.DataLayer.DTO;
+using FeedService.DataLayer.Models;
+using FeedService.Logic.interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedService.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase {
+		private ICrudService<Product, ProductDto> _crudService;
+
+		public ProductController(ICrudService<Product, ProductDto> crudService) {
+			_crudService = crudService;
+		}
+
+		[HttpGet]
+		public IActionResult Get() {
+			return Ok(_crudService.Get());
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(int id) {
+			return Ok(_crudService.Get(id));
+		}
+
+		[HttpPost]
+		public IActionResult Post(Product product) {
+			if (product is null) return BadRequest("product is null!");
+			
+			_crudService.Create(product);
+
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id) {
+			_crudService.Delete(id);
+			return Ok();
+		}
+
+		[HttpPut]
+		public IActionResult Put(int id, ProductDto product) {
+			if (product is null) return BadRequest("product is null!");
+
+			_crudService.Update(id, product);
+
+			return Ok();
+		}
+	}
+}
---
+++ FeedService/DataLayer/DTO/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using FeedService.DataLayer.Interfaces;
+using FeedService.DataLayer.Models;
+
+namespace FeedService.DataLayer.DTO {
+	public class ProductDto: IDto<Product, ProductDto> {
+		public double? Cost { get; set; }
+		public int? ProductTypeId { get; set; }
+
+		public static Product operator +(Product baseType, ProductDto dtoType) {
+			Product result = new();
+
+			result.Id = baseType.Id;
+			result.ProductTypeId = dtoType.ProductTypeId ?? baseType.ProductTypeId;
+			result.Cost = dtoType.Cost ?? baseType.Cost;
+
+			return result;
+		}
+	}
+}
