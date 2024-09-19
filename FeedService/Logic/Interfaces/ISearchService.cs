1dd05c1390b6e6eb60857a276058def078596ef7
H1Kt0 <konovalchuk.denis@gmail.com>
2024-09-19 18:31:07 +0300

    Добавил сервис для поиска по бд Это сделано для того, чтобы не передавать везде контекст бд

---
+++ FeedService/Logic/Interfaces/ISearchService.cs
@@ -0,0 +1,12 @@
+﻿using FeedService.DataLayer.Interfaces;
+using System.Linq.Expressions;
+
+namespace FeedService.Logic.Interfaces {
+	public interface ISearchService {
+		public T Get<T>(Expression<Func<T, bool>> predicate) 
+		where T : class, ITable;
+		
+		public List<T> GetList<T>(Expression<Func<T, bool>> predicate) 
+		where T : class, ITable;
+	}
+}
---
+++ FeedService/Logic/Services/SearchService.cs
@@ -0,0 +1,24 @@
+﻿using FeedService.DataLayer;
+using FeedService.DataLayer.Interfaces;
+using FeedService.Logic.Interfaces;
+using System.Linq.Expressions;
+
+namespace FeedService.Logic.Services {
+	public class SearchService : ISearchService {
+		private static FinlahContext _dbContext = null!;
+
+		public SearchService(FinlahContext context) {
+			_dbContext = context;
+		}
+
+		public T Get<T>(Expression<Func<T, bool>> predicate) 
+		where T: class, ITable {
+			return _dbContext.Set<T>().First(predicate);
+		}
+
+		public List<T> GetList<T>(Expression<Func<T, bool>> predicate)
+		where T: class, ITable {
+			return _dbContext.Set<T>().Where(predicate).ToList();
+		}
+	}
+}
