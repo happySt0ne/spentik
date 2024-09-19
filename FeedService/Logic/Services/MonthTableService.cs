454089ff9c5cac59dca3414c2855fa028a9c65ce
H1Kt0 <konovalchuk.denis@gmail.com>
2024-09-19 18:40:52 +0300

    Убрал тестовые части кода.

--- FeedService/Logic/Services/MonthTableService.cs
+++ FeedService/Logic/Services/MonthTableService.cs
@@ -19,16 +19,7 @@ public class MonthTableService : IMonthTableService {
 		Dictionary<string, List<double>> sums = new(Months.GetMonthLength(month));
 
 		List<DayProduct> dayProducts = GetDayProducts(year, month);
-
-        Console.WriteLine($"pipipicda {dayProducts[0]}");
 		HashSet<string> productTypeNames = GetProductTypeNames(dayProducts);
-
-		if (productTypeNames is null) {
-			Console.WriteLine("anaasdnaskjdnakjsndkjasjdnajskdjsakdnasdasdasdadasdasdasdasdasad");
-		} else {
-			Console.WriteLine($"pizdec {dayProducts.Count} {dayProducts[0]}");
-
-		}
 
 		foreach (var productTypeName in productTypeNames) {
 			var s = dayProducts
@@ -37,9 +28,6 @@ public class MonthTableService : IMonthTableService {
 				.OrderBy(g => g.Key)
 				.Select(g => g.Sum(dp => dp.Product.Cost))
 				.ToList();
-
-			Console.WriteLine($"nigger {s.Count}");
-			Console.WriteLine($"zzzzzzzzzzz {productTypeName}");
 
 			sums.Add(productTypeName, s);
 		}
