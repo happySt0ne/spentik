0c2db41b6288aaa41f0fa0c981f1d85fa00ff774
H1Kt0 <konovalchuk.denis@gmail.com>
2024-09-19 18:26:30 +0300

    Добавил ленивую загрузку

--- FeedService/DataLayer/DbContext.cs
+++ FeedService/DataLayer/DbContext.cs
@@ -3,10 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FeedService.DataLayer {
 	public class FinlahContext : DbContext {
-		public FinlahContext(DbContextOptions<FinlahContext> options) 
-			: base(options) { }
+		public FinlahContext(DbContextOptions<FinlahContext> options)
+				: base(options) {
+			ChangeTracker.LazyLoadingEnabled = true;
+			Database.EnsureCreated();
+		}
 
 		public DbSet<Product> Products { get; set; }
 		public DbSet<ProductType> ProductTypes { get; set; }
+		public DbSet<Day> Day { get; set; }
+		public DbSet<DayProduct> DayProduct { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder
+				.UseLazyLoadingProxies();
+		}
 	}
 }
