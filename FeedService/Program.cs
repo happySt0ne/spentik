82d3563756863ebe07bc34de636b85c911762c96
H1Kt0 <konovalchuk.denis@gmail.com>
2024-10-02 22:16:29 +0300

    Теперь проблема только с доступом к SQL

--- FeedService/Program.cs
+++ FeedService/Program.cs
@@ -4,12 +4,13 @@ using FeedService.Logic.interfaces;
 using FeedService.Logic.Interfaces;
 using FeedService.Logic.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<FinlahContext>(
 	options => options.UseSqlServer(
-		"Server=localhost,1433;Database=Finlah;User=sa;Password=AnalKarnaval228$;TrustServerCertificate=True"));
+		  "Server=localhost,1433;Database=Finlah;User=sa;Password=AnalKarnaval228$;TrustServerCertificate=True"));
 
 builder.Services.AddScoped(typeof(ICrudService<,>), typeof(CrudService<,>));
 
@@ -28,6 +29,9 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod()
             .AllowAnyHeader()));
 
+/* CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture; */
+/* CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture; */
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
@@ -43,5 +47,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+/* using (var scope = app.Services.CreateScope()) */
+/* { */
+/*     var services = scope.ServiceProvider; */
+/*     var context = services.GetRequiredService<FinlahContext>(); */
+/*     context.Database.Migrate(); */
+/* } */
 
 app.Run();
