c62df69751e15752e3838a23b6ad8d0646cc5e5a
H1Kt0 <konovalchuk.denis@gmail.com>
2024-10-03 02:02:20 +0300

    Добавил фронтенд в докер

--- frontend/src/Table.jsx
+++ frontend/src/Table.jsx
@@ -18,7 +18,7 @@ export default function Table() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    fetch(`http://localhost:5033/api/Table/${TODAY}`)
+    fetch(`http://localhost:8080/api/Table/${TODAY}`)
     .then(response => response.json()) 
     .then(data => setData(data))
     .catch(error => console.error(error));
